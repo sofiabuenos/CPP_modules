3346cfb60da7884acfcb38377c2ada4e08b04d4f
Sofia Bueno Silva <sbueno-s@c2r1s2.42porto.com>
2025-05-08 16:48:51 +0100

    cpp00 - ex01

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac >= 2)
+	{
+		for (int i = 1; av[i]; i++ )
+		{
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)toupper(av[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
